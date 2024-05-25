6fda3ff1d6a5f38fd57edb2483a56e9a9e460816
geekclick <anujghom27@gmail.com>
2024-05-25 18:36:11 +0530

    chat feature removed

--- client/src/pages/Chats.tsx
+++ client/src/pages/Chats.tsx
@@ -1,21 +1,21 @@
-import ChatSection from "@/components/Chat/ChatSection";
-import Header from "@/components/Chat/Header";
-import UsersSection from "@/components/Chat/UsersSection";
+// import ChatSection from "@/components/Chat/ChatSection";
+// import Header from "@/components/Chat/Header";
+// import UsersSection from "@/components/Chat/UsersSection";
 
-function Chats() {
-  return (
-    <section className="w-full flex items-center flex-col ">
-      {/* chat header */}
-      <Header />
-      <div className="flex flex-col justify-between items-center">
-        {/* users section */}
-        <UsersSection />
+// function Chats() {
+//   return (
+//     <section className="w-full flex items-center flex-col ">
+//       {/* chat header */}
+//       <Header />
+//       <div className="flex flex-col justify-between items-center">
+//         {/* users section */}
+//         <UsersSection />
 
-        {/* chat section */}
-        <ChatSection />
-      </div>
-    </section>
-  );
-}
+//         {/* chat section */}
+//         <ChatSection />
+//       </div>
+//     </section>
+//   );
+// }
 
-export default Chats;
+// export default Chats;
